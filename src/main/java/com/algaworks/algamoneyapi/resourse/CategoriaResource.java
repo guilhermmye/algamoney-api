29ffb80ad5ac68ffb4870ab70a0898d6a9e2fa19
guilherme.jesus <guilherme.jesus@capgemini.com>
2017-09-28 17:52:22 -0300

    adicionando fail-on-unknown no properties.Inclusão dos metodos criar() e buscarPeloCodigo() na Classe CategoriaResource.

--- src/main/java/com/algaworks/algamoneyapi/resourse/CategoriaResource.java
+++ src/main/java/com/algaworks/algamoneyapi/resourse/CategoriaResource.java
@@ -1,11 +1,19 @@
 package com.algaworks.algamoneyapi.resourse;
 
+import java.net.URI;
 import java.util.List;
 
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.algaworks.algamoneyapi.model.Categoria;
 import com.algaworks.algamoneyapi.repository.CategoriaRepository;
@@ -21,5 +29,26 @@ public class CategoriaResource {
     public List<Categoria> listarTodos(){
         return categoriaRepository.findAll();
     }
+   
+    @PostMapping
+//  @ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<Categoria> criar(@RequestBody Categoria categoria,HttpServletResponse response){
+       Categoria categoriaSalva = categoriaRepository.save(categoria);
+       
+       URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}")
+                   .buildAndExpand(categoriaSalva.getCodigo()).toUri();
+       
+       response.setHeader("Location",uri.toASCIIString());
+       
+       return ResponseEntity.created(uri).body(categoriaSalva);
+    }
+    
+    @GetMapping("/{codigo}")
+    public ResponseEntity<Categoria> buscarPeloCodigo(@PathVariable Long codigo){
+        Categoria categoria = categoriaRepository.findOne(codigo);
+        return categoria != null ? ResponseEntity.ok(categoria) : ResponseEntity.notFound().build();
+        
+    }
+    
 
 }
