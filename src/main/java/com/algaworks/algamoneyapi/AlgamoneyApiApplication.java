07f2cd11a46e81d2ab922fffa214f2a16abaa3bf
guilherme.jesus <guilherme.jesus@capgemini.com>
2017-09-28 16:03:30 -0300

    Criação do model,repository e resourse de Categorias.Adicionando integração com flyway. Adicionando conexão com bando de dados e script de criação da tabela categoria.

--- src/main/java/com/algaworks/algamoneyapi/AlgamoneyApiApplication.java
+++ src/main/java/com/algaworks/algamoneyapi/AlgamoneyApiApplication.java
@@ -1,4 +1,4 @@
-package com.algaworks.algamoneyapi.algamoneyapi;
+package com.algaworks.algamoneyapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
