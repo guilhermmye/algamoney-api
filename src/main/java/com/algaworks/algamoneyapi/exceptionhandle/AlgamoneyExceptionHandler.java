01e8abdb3f4ec4f972ac5e145dd5685ff1f0dfb5
guilhermmye <guilhermmye@gmail.com>
2017-10-13 23:36:42 -0300

    adicionando validação com bean validation

--- src/main/java/com/algaworks/algamoneyapi/exceptionhandle/AlgamoneyExceptionHandler.java
+++ src/main/java/com/algaworks/algamoneyapi/exceptionhandle/AlgamoneyExceptionHandler.java
@@ -1,5 +1,8 @@
 package com.algaworks.algamoneyapi.exceptionhandle;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
@@ -7,9 +10,14 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import java.util.List;
+
 
 @ControllerAdvice
 public class AlgamoneyExceptionHandler extends ResponseEntityExceptionHandler{
@@ -24,7 +32,31 @@ public class AlgamoneyExceptionHandler extends ResponseEntityExceptionHandler{
 		String mensagemUsuario = messageSource.getMessage("mensagem.invalida", null,LocaleContextHolder.getLocale());
 		String mensagemDesenvolvedor = ex.getCause().toString();
 		
-		return handleExceptionInternal(ex,new Erro(mensagemUsuario, mensagemDesenvolvedor), headers, HttpStatus.BAD_REQUEST, request);
+		List<Erro> erros = Arrays.asList(new Erro(mensagemUsuario, mensagemDesenvolvedor));		
+		return handleExceptionInternal(ex,erros, headers, HttpStatus.BAD_REQUEST, request);
+	}
+	
+	@Override
+	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+			HttpHeaders headers, HttpStatus status, WebRequest request) {
+		
+		List<Erro> erros = criaListaErros(ex.getBindingResult());	
+		return handleExceptionInternal(ex,erros, headers, HttpStatus.BAD_REQUEST, request);
+	}
+	
+	private List<Erro> criaListaErros(BindingResult bindResult){
+		List<Erro> erros = new ArrayList<>();
+		
+		for(FieldError fieldError : bindResult.getFieldErrors() ) {
+			
+			String mensagemUsuario = messageSource.getMessage(fieldError, LocaleContextHolder.getLocale());
+			String mensagemDesenvolvedor = fieldError.toString();
+			
+			erros.add(new Erro(mensagemUsuario, mensagemDesenvolvedor));
+			
+		}
+	
+		return erros;
 	}
 	
 	public static class Erro{
